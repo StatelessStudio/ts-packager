f919666d879d98085e390fd49a01f01c19281da8
aimmerman <aimmerman@diversifiedus.com>
2021-10-28 15:37:31 -0400

    Add utils/getBuildDirectory

---
+++ src/utils/get-build-directory.ts
@@ -0,0 +1,19 @@
+import { join as joinPath } from 'path';
+
+/**
+ * Get the build directory from a tsconfig file
+ *
+ * @param tsconfig TSConfig file name
+ * @returns Returns the build directory
+ */
+export function getBuildDirectory(tsconfig: string): string {
+	// eslint-disable-next-line @typescript-eslint/no-var-requires
+	const config = require(joinPath(process.cwd(), tsconfig));
+	const outdir = config?.compilerOptions?.outDir;
+
+	if (!outdir) {
+		throw new Error('Could not get output directory');
+	}
+
+	return outdir;
+}
